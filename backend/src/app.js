beeb155284a9542e218bba4f03d08c69a015bd09
Hritik Chauhan <hritikchji@gmail.com>
2025-11-04 17:22:12 +0530

    add root endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,17 @@ app.use(express.json({ limit: "100kb" }));
 app.use(express.urlencoded({ extended: true, limit: "100kb" }));
 app.use(express.static("public"));
 
+// Root endpoint
+app.get("/", (_, res) => {
+  res.json({
+    message: "AI-Powered Job Recommendation And Application Portal Backend API",
+    version: "1.0.0",
+    endpoints: {
+      health: "/api/v1/health",
+    },
+  });
+});
+
 //routes
 app.use("/api/v1/health", healthcheckRouter);
 app.use("/api/v1/auth", authRouter);
