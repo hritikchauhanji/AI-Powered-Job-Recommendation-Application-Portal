b969ae1a640c9d274f2d5a22ac23221eb30c5a9d
Hritik Chauhan <hritikchji@gmail.com>
2025-11-04 14:27:52 +0530

    implement multer.middleware.js

---
+++ backend/src/middlewares/multer.middleware.js
@@ -0,0 +1,45 @@
+import multer from "multer";
+import path from "path";
+
+const uploadPath =
+  process.env.NODE_ENV === "production" ? "/tmp" : "./public/temp";
+
+// Multer Setup
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+// file filter for profileImage
+const fileFilter = (req, file, cb) => {
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (ext === ".png" || ext === ".jpg" || ext === ".jpeg") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only images are allowed"));
+  }
+};
+
+// file filter for projectFile
+const fileFilterForResumeFile = (req, file, cb) => {
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (ext === ".pdf") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only PDF file is allowed"));
+  }
+};
+
+export const upload = multer({
+  storage,
+  fileFilter,
+});
+
+export const uploadResumeFile = multer({
+  storage,
+  fileFilterForResumeFile,
+});
