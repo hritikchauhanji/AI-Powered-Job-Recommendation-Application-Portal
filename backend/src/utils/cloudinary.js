e8b8d34c91dcb8b811fc08b855d8c2310bc3ff3c
Hritik Chauhan <hritikchji@gmail.com>
2025-11-04 14:37:58 +0530

    implement cloudinary for cloud storage

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,49 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// cloudinary upload
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // file has been uploaded successfull
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    // remove the locally saved temporary file as the upload operation got failed
+    fs.unlinkSync(localFilePath);
+    console.error("Cloudinary Upload Error: ", error.message);
+    return null;
+  }
+};
+
+// delete from cloudinary
+const deleteFromCloudinary = async (publicId) => {
+  try {
+    if (!publicId) return null;
+
+    const response = await cloudinary.uploader.destroy(publicId);
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary, deleteFromCloudinary };
