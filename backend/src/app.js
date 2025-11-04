9b7810b827f6c2e8bd3835d7038523694aa255fd
Hritik Chauhan <hritikchji@gmail.com>
2025-11-04 15:08:07 +0530

    implement health check api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,15 @@
 import express from "express";
-
+import { errorHandler } from "./middlewares/error.middleware.js";
+import healthcheckRouter from "./routes/health.routes.js";
 const app = express();
 
 app.use(express.json({ limit: "100kb" }));
 app.use(express.urlencoded({ extended: true, limit: "100kb" }));
 app.use(express.static("public"));
+
+//routes
+app.use("/api/v1/health", healthcheckRouter);
+
+app.use(errorHandler);
 
 export { app };
