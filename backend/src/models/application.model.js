7097ccde0c13970669d43f32ba9742484708c972
Hritik Chauhan <hritikchji@gmail.com>
2025-11-05 11:08:33 +0530

    create application model with enum

---
+++ backend/src/models/application.model.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+import { AvailableStatus, StatusEnum } from "../constants";
+
+const applicationSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    jobId: {
+      type: Schema.Types.ObjectId,
+      ref: "Job",
+      required: true,
+      index: true,
+    },
+    status: {
+      type: String,
+      enum: AvailableStatus,
+      default: StatusEnum.PENDING,
+      index: true,
+    },
+    coverLetter: {
+      type: String,
+    },
+    appliedOn: {
+      type: Date,
+      default: Date.now,
+      index: true,
+    },
+    reviewedOn: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+// Prevent duplicate applications
+applicationSchema.index({ userId: 1, jobId: 1 }, { unique: true });
+
+export const Application = mongoose.model("Application", applicationSchema);
